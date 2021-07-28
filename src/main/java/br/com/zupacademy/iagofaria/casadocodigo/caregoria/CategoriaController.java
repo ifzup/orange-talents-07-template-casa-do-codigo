ce8e3332eae1af01eda5fa2c65818a324adf78bf
IagoFaria <iago.faria@zup.com.br>
2021-07-28 16:21:27 -0300

    Criação de CategoriaController

--- src/main/java/br/com/zupacademy/iagofaria/casadocodigo/caregoria/CategoriaController.java
+++ src/main/java/br/com/zupacademy/iagofaria/casadocodigo/caregoria/CategoriaController.java
@@ -1,4 +1,21 @@
 package br.com.zupacademy.iagofaria.casadocodigo.caregoria;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categoria")
 public class CategoriaController {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @PostMapping
+    public void cadastrar(@RequestBody @Valid CategoriaRequest categoriaForm){
+        Categoria categoria = categoriaForm.criarCategoria();
+        categoriaRepepository.save(categoria);
+    }
 }
